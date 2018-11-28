651ab7aa377733021dd6ad5707d21cc0b5896195
SAMUEL samuelgutimuñozSsa <samuelgutierrezm@sasahotmail.com>
2018-11-27 23:33:38 -0500

    Add project files.

---
+++ Final_Moviles.Api/Models/Publicacion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Final_Moviles.Api.Models
+{
+    [Table("Publicacion")]
+    public class Publicacion
+    {
+        [Key]
+        public int IdPublicacion { get; set; }
+
+        [MaxLength(250)]
+        public string Descripcion { get; set; }
+
+        public DateTime FechaPublicacion { get; set; }
+
+        public int MeGusta { get; set; }
+        public int MeDisgusta { get; set; }
+        public int VecesCompartido { get; set; }
+    }
+}
